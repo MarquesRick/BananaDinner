5735c8b5ddaabdef16e61b9f7454456e938aa6b8
marques-rick <hmservicostech@outlook.com.br>
2024-08-21 17:25:07 -0300

    feat: add editorconfig, StyleCop.Analyzers and changed Aggregates

--- src/BananaDinner.Domain/Common/Models/AggregateRoot.cs
+++ src/BananaDinner.Domain/Common/Models/AggregateRoot.cs
@@ -2,10 +2,8 @@ namespace BananaDinner.Domain.Common.Models;
 public class AggregateRoot<TId> : Entity<TId>
     where TId : notnull
 {
-    protected AggregateRoot(TId id) : base(id)
+    protected AggregateRoot(TId id)
+        : base(id)
     {
-
-
     }
-
 }
